75b690c984219e132cd8b85b7b77b471a8bbbfdb
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2018-01-19 21:15:55 +0900

    複数アクターのケースを確認

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,24 @@
 package sample
 
 import akka.actor.{ ActorSystem, Props }
-import sample.GetSpeed.Event
+import sample.GetSpeed.{ Event => GetSpeedEvent }
+import sample.GetTime.{ Event => GetTimeEvent }
+import sample.GetNumberPlate.{ Event => GetNumberPlateEvent }
 
 object Main extends App {
   val system: ActorSystem = ActorSystem("traffic")
 
   val getSpeed = system.actorOf(Props[GetSpeed], "getSpeed")
 
-  getSpeed ! Event("40km/h", None)
+  getSpeed ! GetSpeedEvent("40km/h", None)
+
+  val getTime = system.actorOf(Props[GetTime], "getTime")
+
+  getTime ! GetTimeEvent(java.time.LocalDate.now.toString, None)
+
+  val getNumberPlate = system.actorOf(Props[GetNumberPlate], "getNumberPlate")
+
+  getNumberPlate ! GetNumberPlateEvent("な17-17", None)
 
   system.terminate()
 }
